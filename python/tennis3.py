5fe8c0263205c32f1471d3ebd812baec51a59f41
Enzo_Tuel <enzo.tueleau@epsi.fr>
2024-07-21 22:30:08 +0200

    refacto tennis3.py

--- python/tennis3.py
+++ python/tennis3.py
@@ -1,29 +1,34 @@
-# -*- coding: utf-8 -*-
 class TennisGame3:
-    def __init__(self, player1Name, player2Name):
-        # TODO nommage de variable peu explicite, non conventionnel avec le langage également -> Trouver un meilleur nom
-        self.p1N = player1Name
-        self.p2N = player2Name
-        # TODO appeler p1 player_1_score par exemple
-        self.p1 = 0
-        self.p2 = 0
-    # TODO Nom de paramètre peu explicite -> transformer n en name
-    def won_point(self, n):
-        if n == "player1":
-            self.p1 += 1
+    def __init__(self, player1_name, player2_name):
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.player1_score = 0
+        self.player2_score = 0
+
+    def won_point(self, player_name):
+        if player_name == self.player1_name:
+            self.player1_score += 1
+        elif player_name == self.player2_name:
+            self.player2_score += 1
         else:
-            self.p2 += 1
-    # TODO Renommer la fonction afin de comprendre son usage ex : return_game_score
-    def score(self):
-        # TODO Créer des variables contenant les expressions booléennes
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            # TODO p et s ? Variables non explicites
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            print('test')
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+            raise ValueError("Invalid player name.")
+    
+    def get_score(self):
+        score_map = ["Love", "Fifteen", "Thirty", "Forty"]
+        both_scores_under_four = self.player1_score < 4 and self.player2_score < 4
+        total_points_under_six = self.player1_score + self.player2_score < 6
+        
+        if both_scores_under_four and total_points_under_six:
+            player1_score_str = score_map[self.player1_score]
+            player2_score_str = score_map[self.player2_score]
+            if self.player1_score == self.player2_score:
+                return f"{player1_score_str}-All"
+            return f"{player1_score_str}-{player2_score_str}"
         else:
-            if (self.p1 == self.p2):
+            if self.player1_score == self.player2_score:
                 return "Deuce"
-            s = self.p1N if self.p1 > self.p2 else self.p2N
-            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+            leading_player = self.player1_name if self.player1_score > self.player2_score else self.player2_name
+            score_difference = abs(self.player1_score - self.player2_score)
+            if score_difference == 1:
+                return f"Advantage {leading_player}"
+            return f"Win for {leading_player}"
--- python/tennis_unittest.py
+++ python/tennis_unittest.py
@@ -65,7 +65,7 @@ class TestTennis(unittest.TestCase):
         for testcase in test_cases:
             (p1Points, p2Points, score, p1Name, p2Name) = testcase
             game = play_game(TennisGame3, p1Points, p2Points, p1Name, p2Name)
-            self.assertEqual(score, game.score())
+            self.assertEqual(score, game.get_score())
 
 
     def test_Score_Game5(self):
